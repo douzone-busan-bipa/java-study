d638ec94bccef5fbd513e02b6b732c7f571d569a
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-04-27 11:18:01 +0900

    접근 제어 키워드

--- chapter03/src/main/java/chapter03/Goods.java
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -15,6 +15,10 @@ public class Goods {
 		return price;
 	}
 	public void setPrice(int price) {
+		// 데이터 보호
+		if(price < 0) {
+			price = 0;
+		}
 		this.price = price;
 	}
 	public int getCountStock() {
--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -5,7 +5,7 @@ public class GoodsApp {
 	public static void main(String[] args) {
 		Goods goods = new Goods();
 		goods.setName("nikon");
-		goods.setPrice(400000);
+		goods.setPrice(-1);
 		goods.setCountSold(50);
 		goods.setCountStock(30);
 	}
